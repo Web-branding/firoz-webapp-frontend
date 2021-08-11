27b455ee47cfee4d8e66517a6d23a3ab9fb8c4bd
SAbee Lap <sabeerali099@gmail.com>
2021-08-11 12:23:40 +0530

    basic project set up

---
+++ src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="container bg-success">
+      <h2>Bootstrap 5 is working.....</h2>
+
+      {/* 
+      ProjectContainer will contain header and footer
+      */}
+      {/* Basic Structure */}
+      {/* <ProjectContainer>
+        <HomePage />
+        <Page2 />
+        <Page3 />
+        <Page4 />
+      </ProjectContainer> */}
+    </div>
+  );
+}
+
+export default App;
